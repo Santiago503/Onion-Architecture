8c82878fd5f38659dd0857962a22328c59e82b9b
Santiago Encarnacion <Santiago.503.es@gmail.com>
2022-08-17 13:48:42 -0400

    Add NuGet and ServicesExtension, MadiatR, AutoMapper

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection service)
+        { 
+            service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+        }
+
+    }
+}

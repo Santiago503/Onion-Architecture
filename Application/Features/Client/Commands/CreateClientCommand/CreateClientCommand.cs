58c6a423af605d4e4088f1f590f693883d3d7991
Santiago Encarnacion <Santiago.503.es@gmail.com>
2022-08-27 21:35:50 -0400

    added: first entity and class global response

---
+++ Application/Features/Client/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -0,0 +1,15 @@
+﻿using Application.Mappings;
+using MediatR;
+
+namespace Application.Features.Client.Commands.CreateClientCommand
+{
+    public class CreateClientCommand : IRequest<Response<int>>
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
